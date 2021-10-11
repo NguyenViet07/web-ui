cb9c54cf8c49ee519beabc49be4de847d8813b8e
tuanta123 <tuan.ta@techup.vn>
2021-10-11 20:33:22 +0700

    them home

--- src/View/Home.js
+++ src/View/Home.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import sections
 
+import SLideHome from "../components/homes/SlideHome";
+import AlbumNew from "../components/homes/AlbumNew";
+import SongNew from "../components/homes/SongNew";
+import SidebarHome from "../components/homes/Sidebar";
 
 const Home = () => {
   return (
     <>
-        <div>
-            Home
-        </div>
+      {/* <Hero className="illustration-section-01" /> */}
+      <SLideHome />
+      <Container>
+        <Row>
+          <Col md={8}>
+            <AlbumNew></AlbumNew>
+            <img src="/imgs/qc.png" alt="" className="w-100" />
+            <SongNew></SongNew>
+          </Col>
+          <Col md={3}>
+            <SidebarHome />
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <img src="/imgs/qc.png" alt="" className="w-100 mb-4" />
+      </Container>
     </>
   );
 };
---
+++ src/components/homes/SlideHome.js
@@ -0,0 +1,77 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+const SLideHome = ({ data }) => {
+  return (
+    <>
+      <div className="slide-album">
+        <div className="container slide-home">
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="album-item">
+                <a href="#" title="xxxx">
+                  <img src="/imgs/147805.jpg" alt="xx" className="w-100" />
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="album-item">
+                <a href="#" title="xxxx">
+                  <img src="/imgs/147805.jpg" alt="xx" className="w-100" />
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="album-item">
+                <a href="#" title="xxxx">
+                  <img src="/imgs/147805.jpg" alt="xx" className="w-100" />
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="album-item">
+                <a href="#" title="xxxx">
+                  <img src="/imgs/147805.jpg" alt="xx" className="w-100" />
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="album-item">
+                <a href="#" title="xxxx">
+                  <img src="/imgs/147805.jpg" alt="xx" className="w-100" />
+                </a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="album-item">
+                <a href="#" title="xxxx">
+                  <img src="/imgs/147805.jpg" alt="xx" className="w-100" />
+                </a>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SLideHome;
